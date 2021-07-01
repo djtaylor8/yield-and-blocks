75aaa1ac2d5de927b454804471e6abe36a777381
DJ Taylor <dtaylor88@gmail.com>
2021-06-30 19:38:51 -0700

    commit tree 381a908f2115dd9fd9e5f158c510a1c464268a33

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,20 @@
-def hello_t
-
-end
-
-# call your method here!
+  
+def hello_t(names)
+    if block_given?
+      i = 0
+      while i < names.length
+        yield (names[i])
+        i += 1
+      end
+      names
+    else
+      puts "Hey! No block was given!"
+    end
+  end
+  
+  # hello_t(["Tim", "Tom", "Jim"]) do |name|
+  #   if name.start_with?("T")
+  #     puts "Hi, #{name}"
+  #   end
+  # end
 
